ddba8fcdc87cde05f0b21ee3383e2e1f3f0ed62a
ihsan <ihsanmpm@gmail.com>
2018-01-09 09:26:45 +0530

    Add project files.

---
+++ DL list Insertion Operation/DL list Insertion Operation.cpp
@@ -0,0 +1,43 @@
+// DL list Insertion Operation.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+//insert link at the first location
+void insertFirst(int key, int data) {
+
+	//create a link
+	struct node *link = (struct node*) malloc(sizeof(struct node));
+	link->key = key;
+	link->data = data;
+
+	if (isEmpty()) {
+		// make it the last link
+		last = link;
+	}
+	else
+	{
+		//update first prev link
+		head->prev = link;
+	}
+
+	//point it to old first link
+	link->next = head;
+
+	//point first to new first link
+	head = link;
+
+}
+
+
+
+
+
+
+int main()
+{
+    return 0;
+}
+
